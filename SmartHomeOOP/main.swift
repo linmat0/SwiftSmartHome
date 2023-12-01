f6f9e836179984460dd0f28c20546083d5b287c5
Matthew Lin <Lin.M@etoncollege.org.uk>
2023-12-01 09:56:54 +0000

    Added some code

--- SmartHomeOOP/main.swift
+++ SmartHomeOOP/main.swift
@@ -7,5 +7,42 @@
 
 import Foundation
 
-print("Hello, World!")
+class Home {
+    var internet: Bool
+    var temperature: Float
+    var size: Float
+    
+    init(internet: Bool, temperature: Float, size: Float) {
+        self.internet = internet
+        self.temperature = temperature
+        self.size = size
+    }
+}
 
+class Thermostat {
+    var currentTemperature: Float
+    var targetTemperature: Float
+    var stateOn: Bool
+    
+    init(currentTemperature: Float, targetTemperature: Float, stateOn: Bool) {
+        self.currentTemperature = currentTemperature
+        self.targetTemperature = targetTemperature
+        self.stateOn = stateOn
+    }
+    
+    func changeTargetTemp(temp: Float) {
+        self.targetTemperature = temp
+        print("Temperature set to \(temp)")
+        
+    }
+    
+    func changeState() {
+        if self.stateOn == true {
+            self.stateOn = false
+        } else {
+            self.stateOn = true
+        }
+        print("Thermostat is \(self.stateOn)")
+    }
+    
+}
